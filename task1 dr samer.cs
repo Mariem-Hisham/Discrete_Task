bcd39d92488008559583cf2078e2df884532baa5
Mariem-Hisham <121406173+Mariem-Hisham@users.noreply.github.com>
2022-12-25 23:47:36 +0200

    Add files via upload

---
+++ task1 dr samer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace perfectNumber
+{
+    class program
+    {
+        static bool Isperfect(int number)
+        {
+           
+         
+         
+            
+          
+            
+            {
+                for (int x = 1; x <= i; x++)
+                {
+                    if (i %x == 0)
+                        sum++;
+                }
+                if (sum == 2)
+                {
+                    Console.WriteLine(i);
+                    sum = 0;
+                }
+                else
+                
+                   
+                   
+                }
+            }
+        }
+    }
+}
+
+
+
---
+++ task2 dr samer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace perfectNumber
+{
+    class program
+    {
+        static bool Isprefect(int number)
+        {
+            int sum = 0;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0) sum = sum + i;
+            }
+            if (sum == number) return true;
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            int x, y;
+            Console.WriteLine("please enter tne first number");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("please enter the second number");
+            y = int.Parse(Console.ReadLine());
+            for (int i = x; i <= y; i++)
+            {
+                if (Isprefect(i)) Console.WriteLine("{0}", i);
+            }
+        }
+    }
+}
